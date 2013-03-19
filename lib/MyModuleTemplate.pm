14a4c265e6ec4b4c20dbfee634c9a71c2b48e73d
Nick Patch <patch@cpan.org>
2013-03-18 22:38:35 -0400

    new project

---
+++ lib/MyModuleTemplate.pm
@@ -0,0 +1,51 @@
+package MyModuleTemplate;
+
+use v5.6;
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = '0.00_1';
+
+1;
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+MyModuleTemplate - ...
+
+=head1 VERSION
+
+This document describes MyModuleTemplate version 0.00_1.
+
+=head1 SYNOPSIS
+
+    use MyModuleTemplate;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 SEE ALSO
+
+=over
+
+=item * ...
+
+=back
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+© 2013 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
